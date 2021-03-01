8ad8f0c577bb68ae9d50fc514a080f6ac4d19973
peter <peter@ezidol.localdomain>
2021-03-01 17:11:26 +0900

    heap-1

---
+++ BOF/heap-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) {
+    char *input = malloc(40);
+    char *hello = malloc(40);
+
+    memset(input, 0, 40);
+    memset(hello, 0, 40);
+
+    strcpy(hello, "HI!");
+    read(0, input, 100);
+    
+    printf("Input: %s\n", input);
+    printf("hello: %s\n", hello);
+}
