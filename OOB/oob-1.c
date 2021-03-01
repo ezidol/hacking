6c9b891aa40176cfbf53626d98a8a5b63e25f5d0
ezidol <goodyoungmin@naver.com>
2021-03-01 17:46:10 +0900

    oob-1

---
+++ OOB/oob-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int win;
+    int idx;
+    int buf[10];
+
+    printf("Which index? ");
+    scanf("%d", &idx);
+
+    printf("Value: ");
+    scanf("%d", &buf[idx]);
+
+    printf("idx: %d, value: %d\n", idx, buf[idx]);
+
+    if (win == 31337) {
+        printf("SUCCESS!");
+    }
+}
